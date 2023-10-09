f82e2d2910d9e7f8a7dd13f69ad99ce922ee6e82
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-10-09 13:02:10 +0100

    Crafting Station
    Started work on the crafting station and set up the UI that will show all of the items that will be needed to craft the given item.

---
+++ Harvester/Assets/Scripts/Crafting Stations/CraftingStationObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingStationObject : MonoBehaviour
+{
+    [Header("Station ID")]
+    public CraftingStationData stationData;
+    public int stationID;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
