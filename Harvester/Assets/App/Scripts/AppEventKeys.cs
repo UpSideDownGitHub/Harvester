bd3e43eefd48a939e1a804c144f049317e1548a9
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-10-28 19:31:28 +0100

    Removed Lobby Stuff
    relised that i did not need any of that lobby stuff and it was all a waste of time

--- Harvester/Assets/App/Scripts/AppEventKeys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Assets.App.Scripts
-{
-    public struct AppEventKeys
-    {
-        public static string showLobbyView = "app.showLobbyView";
-        public static string hideLobbyView = "app.hideLobbyView";
-    }
-}
